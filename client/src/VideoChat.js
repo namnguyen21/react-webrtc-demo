2b9ca155dd806dc1cb14555600a227948273ae32
unknown <namdoannguyen@gmail.com>
2021-04-14 16:41:24 -0700

    enabled video and voice chat

--- client/src/VideoChat.js
+++ client/src/VideoChat.js
@@ -6,7 +6,130 @@ import Peer from "peerjs";
 const CONNECTION_ENDPOINT = "http://localhost:3001";
 
 export default function VideoChat() {
-  return <div></div>;
+  const { roomId } = useParams();
+  const socketRef = useRef();
+  // arr of {} containing audio and video streams
+  const [peers, setPeers] = useState({});
+  const [myStream, setMyStream] = useState({
+    id: undefined,
+    audio: undefined,
+    video: undefined,
+  });
+
+  useEffect(async () => {
+    socketRef.current = socketIO(CONNECTION_ENDPOINT);
+    let videoStream, audioStream;
+    try {
+      videoStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+    } catch (videoErr) {
+      console.log(videoErr);
+    }
+    try {
+      audioStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+    } catch (audioErr) {
+      console.log(audioErr);
+    }
+
+    // creating 1st person peer
+    const peer = new Peer(undefined);
+    peer.on("open", (myId) => {
+      setMyStream((data) => {
+        return {
+          ...data,
+          id: myId,
+          video: videoStream ? videoStream : undefined,
+          audio: audioStream ? audioStream : undefined,
+        };
+      });
+      socketRef.current.emit("join-room", { roomId, userId: myId });
+
+      // when new person joins room
+      socketRef.current.on("user-connected", (userId) => {
+        // call new user
+        const outgoingCall = peer.call(userId, audioStream, {
+          metadata: { type: "audio", callerId: myId },
+        });
+        outgoingCall.on("stream", (otherStream) => {
+          setPeers((peers) => {
+            const newState = { ...peers };
+            if (userId in newState) {
+              newState[userId].audio = otherStream;
+            } else {
+              newState[userId] = {
+                id: userId,
+                audio: otherStream,
+                video: undefined,
+              };
+            }
+            return newState;
+          });
+        });
+      });
+      // to receive a call
+      peer.on("call", (incomingCall) => {
+        const { callerId, type } = incomingCall.metadata;
+        incomingCall.answer(audioStream);
+        incomingCall.on("stream", (incomingStream) => {
+          const newPeer =
+            callerId in peers
+              ? peers[callerId]
+              : { id: callerId, audio: undefined, video: undefined };
+          // determine which type of call
+          switch (type) {
+            case "audio":
+              newPeer.audio = incomingStream;
+              break;
+            case "video":
+              newPeer.video = incomingStream;
+          }
+          setPeers((peers) => {
+            const newState = { ...peers };
+            newState[callerId] = newPeer;
+            return newState;
+          });
+        });
+      });
+    });
+  }, []);
+
+  function renderPeerAudios() {
+    const peerObjects = Object.keys(peers);
+    if (peerObjects.length === 0) return null;
+    return peerObjects.map((id) => {
+      if (peers[id].audio) {
+        return <Video key={id} stream={peers[id].audio} />;
+      }
+    });
+  }
+
+  function renderPeerVideos() {
+    const peerObjects = Object.keys(peers);
+    if (peerObjects.length === 0) return null;
+    return peerObjects.map((id) => {
+      if (peers[id].video) {
+        return <Video key={id} stream={peers[id].video} />;
+      }
+    });
+  }
+  console.log(peers);
+  return (
+    <div>
+      <div>
+        {myStream.audio ? <Audio stream={myStream.audio} /> : null}
+        {renderPeerAudios()}
+      </div>
+      <div>
+        {myStream.video ? (
+          <Video className="something" stream={myStream.video} />
+        ) : null}
+        {renderPeerVideos()}
+      </div>
+    </div>
+  );
 }
 
 function Video({ stream }) {
@@ -19,4 +142,15 @@ function Video({ stream }) {
   }, [videoRef.current]);
 
   return <video ref={videoRef} />;
+}
+
+function Audio({ stream }) {
+  const audioRef = useRef();
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.srcObject = stream;
+      audioRef.current.play();
+    }
+  }, [audioRef.current]);
+  return <audio ref={audioRef} autoPlay />;
 }
